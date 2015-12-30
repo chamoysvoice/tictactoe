7b8dc10b6842839c1f60ec2b42b030332ac2e3c3
Jesus Leal <chamoysvoice@gmail.com>
2015-12-29 18:31:25 -0600

    Added animations

--- app/src/main/java/com/jesusgarlea/tictactoe/MainActivity.java
+++ app/src/main/java/com/jesusgarlea/tictactoe/MainActivity.java
@@ -2,8 +2,27 @@ package com.jesusgarlea.tictactoe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+    private int player = 1;
+    private int[] fields = {0,0,0,0,0,0,0,0,0};
+
+    public void showXY(View view){
+        ImageView animated = (ImageView) view;
+        animated.setAlpha(0f);
+        animated.setRotationX(0l);
+        if (player == 1) {
+            animated.setImageResource(R.drawable.x);
+            player = 2;
+        } else {
+            animated.setImageResource(R.drawable.o);
+            player = 1;
+        }
+        animated.animate().alpha(1f).rotationXBy(360l).setDuration(700);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
